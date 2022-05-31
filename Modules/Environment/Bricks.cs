3841cafa6263758af119cf44a2094477736f10b2
AidanTMcCormack <Fastmapler@users.noreply.github.com>
2022-05-31 16:33:23 -0400

    Boxes

--- Modules/Environment/Bricks.cs
+++ Modules/Environment/Bricks.cs
@@ -8,3 +8,12 @@ datablock fxDTSBrickData(brickMMBrickGenericData)
 	hasPrint = 1;
 	printAspectRatio = "ModTer";
 };
+
+datablock fxDTSBrickData(brickMMBrickBoxData)
+{
+	brickFile = "Add-Ons/Gamemode_MapleMiners/Modules/Environment/Bricks/Box.blb";
+	category = "Special";
+	subCategory = "Maple Miners";
+	uiName = " Box";
+	iconName = "Add-Ons/Gamemode_MapleMiners/Modules/Environment/Bricks/BrickGeneric";
+};
