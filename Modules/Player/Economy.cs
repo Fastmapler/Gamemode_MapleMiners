12972a7903207425dfc853aeaca94712e64c4e1c
AidanTMcCormack <Fastmapler@users.noreply.github.com>
2022-06-02 12:15:56 -0400

    Item purchasing

--- Modules/Player/Economy.cs
+++ Modules/Player/Economy.cs
@@ -118,3 +118,97 @@ function GameConnection::PurchaseItem(%client, %item)
         commandToClient(%client,'messageBoxYesNo',"Purchasing", "[" @ %item.uiName @ "]<br>Purchase cost:<br>---<br>" @ %text @ "---<br>Purchase this item?", 'PurchaseItemAccept','PurchaseItemCancel');
     }
 }
+
+function ServerCmdPurchaseItemAccept(%client)
+{
+    if (!isObject(%player = %client.player))
+        return;
+
+    %costData = $MM::ItemCost[%client.selectedPurchaseItem.getName()];
+
+    for (%i = 0; %i < getFieldCount(%costData); %i += 2)
+    {
+        if (%client.MM_Materials[getField(%costData, %i + 1)] < getField(%costData, %i))
+        {
+            %client.chatMessage("You need more " @ getField(%costData, %i + 1) @ "!");
+            ServerCmdCraftItemCancel(%client);
+            return;
+        }
+    }
+
+    for (%i = 0; %i < getFieldCount(%costData); %i += 2)
+        %client.MM_Materials[getField(%costData, %i + 1)] -= getField(%costData, %i);
+
+    %item = new Item()
+    {
+        datablock = %client.selectedPurchaseItem;
+        static    = "0";
+        position  = vectorAdd(%player.getPosition(), "0 0 1");
+        craftedItem = true;
+    };
+
+    ServerCmdPurchaseItemCancel(%client);
+}
+
+function ServerCmdPurchaseItemCancel(%client)
+{
+    %client.selectedPurchaseItem = "";
+}
+
+package MM_Economy
+{
+    function Armor::onCollision(%data,%this,%col,%vec,%vel)
+	{
+		if(%col.getClassName() $= "Item")
+		{
+            if (isObject(%col.spawnBrick) && $MM::ItemCost[%col.getDatablock().getName()] !$= "")
+            {
+                if(!%this.client.messagedAboutCTPU)
+                {
+                    messageClient(%this.client,'',"This item must be purchased. Click it with an empty hand to see its price and buy it.");
+                    %this.client.messagedAboutCTPU = 1;
+                }
+                return 0;
+            }
+            else
+            {
+                %this.pickup(%col);
+                return 0;
+            }
+                
+		}
+		return Parent::onCollision(%data,%this,%col,%vec,%vel);
+	}
+    function Armor::onTrigger(%data, %obj, %trig, %tog)
+	{
+		if(isObject(%client = %obj.client))
+		{
+			if(%trig == 0 && %tog && !isObject(%obj.getMountedImage(0)))
+			{
+				%eye = %obj.getEyePoint();
+				%dir = %obj.getEyeVector();
+				%for = %obj.getForwardVector();
+				%face = getWords(vectorScale(getWords(%for, 0, 1), vectorLen(getWords(%dir, 0, 1))), 0, 1) SPC getWord(%dir, 2);
+				%mask = $Typemasks::FxBrickObjectType | $Typemasks::TerrainObjectType | $TypeMasks::ItemObjectType;
+				%ray = containerRaycast(%eye, vectorAdd(%eye, vectorScale(%face, 5)), %mask, %obj);
+				if(isObject(%hit = firstWord(%ray)) && %hit.getClassName() $= "Item")
+				{
+                    %itemData = %hit.getDataBlock();
+					for(%x = 0; %x < %data.maxTools; %x++)
+                    {
+                        if(isObject(%obj.tool[%x]) && %obj.tool[%x].getID() == %itemData.getID())
+						{
+							%copy = 1;
+							break;
+						}
+                    }
+
+                    if(%hit.canPickup && isObject(%hit.spawnBrick) && $MM::ItemCost[%itemData.getName()] !$= "") // && !%copy
+                        %client.PurchaseItem(%itemData);
+				}
+			}
+		}
+		Parent::onTrigger(%data, %obj, %trig, %tog);
+	}
+};
+activatePackage("MM_Economy");
