3e352065cb0ad181fd0ca97b22f4165a32939a01
AidanTMcCormack <Fastmapler@users.noreply.github.com>
2022-06-13 13:11:13 -0400

    Crystal + Orb model

--- Modules/Environment/Bricks.cs
+++ Modules/Environment/Bricks.cs
@@ -17,3 +17,21 @@ datablock fxDTSBrickData(brickMMBrickBoxData)
 	uiName = "Box";
 	iconName = "Add-Ons/Gamemode_MapleMiners/Modules/Environment/Bricks/BrickGeneric";
 };
+
+datablock fxDTSBrickData(brickMMOrbData)
+{
+	brickFile = "Add-Ons/Gamemode_MapleMiners/Modules/Environment/Bricks/Orb.blb";
+	category = "Special";
+	subCategory = "Maple Miners";
+	uiName = "Orb";
+	iconName = "Add-Ons/Gamemode_MapleMiners/Modules/Environment/Bricks/BrickGeneric";
+};
+
+datablock fxDTSBrickData(brickMMCrystalData)
+{
+	brickFile = "Add-Ons/Gamemode_MapleMiners/Modules/Environment/Bricks/Crystal.blb";
+	category = "Special";
+	subCategory = "Maple Miners";
+	uiName = "Crystal";
+	iconName = "Add-Ons/Gamemode_MapleMiners/Modules/Environment/Bricks/BrickGeneric";
+};
