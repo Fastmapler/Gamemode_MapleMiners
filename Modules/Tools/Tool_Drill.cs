d77e0fa82ce32fb5a293f02ac90378d8bfaf2978
AidanTMcCormack <Fastmapler@users.noreply.github.com>
2022-06-30 16:37:51 -0400

    Drills

--- Modules/Tools/Tool_Drill.cs
+++ Modules/Tools/Tool_Drill.cs
@@ -90,3 +90,68 @@ function MM_bsmRefineFuel::onUserMove(%obj, %client, %id, %move, %val)
 
 	Parent::onUserMove(%obj, %client, %id, %move, %val);
 }
+
+//Drill
+datablock StaticShapeData(MMDrillStatic)
+{
+    shapeFile = "./Shapes/Drill.dts";
+};
+
+function MMDrillStatic::onAdd(%this, %obj)
+{
+
+    Parent::onAdd(%this, %obj);
+}
+
+function StaticShape::DrillTick(%obj)
+{
+    //drawArrow(%obj.getPosition(), vectorNormalize(%obj.getForwardVector()), "1 0 0 1", 2, 1, "0 0 0").schedule(2000, "delete");
+    //drawArrow(%obj.getPosition(), vectorNormalize(%obj.getUpVector()), "0 1 0 1", 2, 1, "0 0 0").schedule(2000, "delete");
+    //drawArrow(%obj.getPosition(), vectorNormalize(%obj.getLeftVector()), "0 0 1 1", 2, 1, "0 0 0").schedule(2000, "delete");
+
+    %radius = 2;
+    for (%x = %radius * -1; %x <= %radius; %x++)
+    {
+        for (%y = %radius * -1; %y <= %radius; %y++)
+        {
+            %target = vectorAdd(%obj.getPosition(), vectorNormalize(%obj.getForwardVector()));
+            %target = vectorAdd(%target, vectorScale(vectorNormalize(%obj.getLeftVector()), %x));
+            %target = vectorAdd(%target, vectorScale(vectorNormalize(%obj.getUpVector()), %y));
+            %target = roundVector(%target);
+
+            RevealBlock(%target);
+			if (isObject(%brick = $MM::BrickGrid[%target]))
+				%brick.MineDamage(999999);
+                
+            //drawArrow2(%obj.getPosition(), %target, "1 1 1 1", 0.5, "0 0 0").schedule(2000, "delete");
+        }
+    }
+}
+
+function StaticShape::LerpMove(%obj, %target, %time, %ticks)
+{
+    if (%ticks < 1)
+        %ticks = 10;
+
+    %time /= %ticks;
+
+    %move = vectorScale(vectorSub(%obj.getPosition(), %target), 1 / %ticks);
+    %obj.LerpMoveTick(%move, %time, %ticks - 1);
+}
+
+function StaticShape::LerpMoveTick(%obj, %move, %time, %movesLeft)
+{
+    cancel(%obj.LerpMoveSchedule);
+
+    %pos = %obj.getPosition();
+    %obj.setTransform(vectorAdd(getWords(%pos, 0, 2), %move) SPC getWords(%pos, 3));
+
+    if (%movesLeft > 0)
+        %obj.LerpMoveSchedule = %obj.schedule(%time, "LerpMoveTick", %move, %time, %movesLeft - 1);
+}
+
+//$test.setTransform(vectorAdd($test.getPosition(), $test.faceDirection()) SPC getWords($test.getTransform(),3,7));
+//$Test.setTransform(vectorAdd($test.getPosition(),$test.getForwardVector()) SPC getWords($test.getTransform(),3,7));
+//$test.setTransform("0 0 5121" SPC getWords(MatrixCreateFromEuler("0 0 1.5708"), 3, 7)); $test.drillTick();
+//$test.drillTick(); $test.setTransform(vectorAdd($test.getPosition(),$test.getForwardVector()) SPC getWords(MatrixCreateFromEuler("0 0 0"), 3, 7));
+//$test = new StaticShape() {datablock = MMDrillStatic; };
