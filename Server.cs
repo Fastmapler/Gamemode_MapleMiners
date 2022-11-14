fd2f57a9df9b9bad94b71293cf8fc138212e3d5b
AidanTMcCormack <Fastmapler@users.noreply.github.com>
2022-11-13 23:24:03 -0500

    Trace test

--- Server.cs
+++ Server.cs
@@ -6,3 +6,10 @@ for (%i = 0; %i < getWordCount($EOTW::Modules); %i++)
 
 //One of our goals is to keep the code as organized as possible. We split each section of code into their own
 //folders. Even though loading more files may take longer, the better code organization and readability is worth it.
+
+function EnableTracing()
+{
+	if (isFunction("trace2"))
+		trace2(1);
+}
+EnableTracing();
